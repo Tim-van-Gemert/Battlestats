3ac34e6c9347d1a3af0511181a7f2150aa369cfc
Tim van Gemert <timvangemert@outlook.com>
2023-06-15 16:30:45 +0200

    made player selection more responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
   const [game, setGame] = useState('bf2042')
 
   useEffect(()=>{
-    console.log(userName, platform, game)
   },[userName, platform, game])
 
   return (
@@ -16,9 +15,9 @@ export default function Home() {
       <Head>
         <title>BattleStat</title>
       </Head>
-      <div className={`xl:w-[1200px]  z-20 mt-12 flex flex-col gap-6  h-[100%] min-h-[100%] pb-20`}>
-        <div className={` grid grid-cols-1 sm:grid-cols-7 relative z-20 gap-6 xl:h-[100px]`}>
-          <input className={` bg-[#1E1E27] col-span-2  rounded-[5px] h-fit p-4 gap-6`} placeholder="Your username" onChange={(event) => setUsernameName(event.target.value)}/>
+      <div className={`xl:w-[1200px] w-[100%]  z-20 mt-12 flex flex-col gap-6  h-[100%] min-h-[100%] pb-20`}>
+        <div className={` grid grid-cols-1 sm:grid-cols-7 w-[100%] relative z-20 gap-6 xl:h-[100px]`}>
+          <input className={` bg-[#1E1E27] sm:col-span-2  rounded-[5px] h-fit p-4 gap-6`} placeholder="Your username" onChange={(event) => setUsernameName(event.target.value)}/>
           <div className=" bg-[#1E1E27]  rounded-[5px] h-fit p-4 gap-6 ">
             <select className="bg-[#1E1E27] w-full" name="platform" id="platform"   onChange={(event) => setPlatform(event.target.value)}>
               <option value="pc">PC</option>
