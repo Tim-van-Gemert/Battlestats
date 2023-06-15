f16a527b3b138914667e7a28c7fde3e0bc26f5df
Tim van Gemert <timvangemert@outlook.com>
2023-06-15 12:01:43 +0200

    fixed build issue

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className="text-white  flex-row py-5 flex xl:w-[1200px] w-full items-center ">
             <div className="flex flex-col relative z-30 ">
-                <a className="flex justify-start uppercase text-[28px]" href="/">BattleStat</a>
+                <Link className="flex justify-start uppercase text-[28px]" href="/">BattleStat</Link>
                 <div className="flex justify-start uppercase group text-[12px] w-fit hover:cursor-pointer text-[#B4B4B4] flex flex-col ">
                     <a target="blank" href={"https://www.gametools.network/stats/"}>A Gametools clone</a>
                     <div className="w-0 group-hover:w-full bg-white h-[2px] transition-all" ></div>
